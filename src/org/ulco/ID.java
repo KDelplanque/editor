fe395b053f698467ad9444f4b866d65e851aa91c
kdelplanque <k.delplanque@gmail.com>
2015-11-24 13:52:21 +0100

    Quatrième question

--- src/org/ulco/ID.java
+++ src/org/ulco/ID.java
@@ -2,4 +2,18 @@ package org.ulco;
 
 public class ID {
     static public int ID = 0;
+
+    /** Constructeur privé */
+    private ID()
+    {
+        ID++;
+    }
+
+    /** Instance unique pré-initialisée */
+    private static ID INSTANCE = new ID();
+
+    /** Point d'accès pour l'instance unique du singleton */
+    public static ID getInstance()
+    {	return INSTANCE;
+    }
 }
